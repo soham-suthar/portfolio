78d05285e1c8055794056581deea9e5b91003fce
soham-suthar <sohamnishithsutharrr@gmail.com>
2026-05-15 23:06:59 +0530

    Add initial portfolio structure with HTML, CSS, JS, images, and settings

---
+++ main.js
@@ -0,0 +1,63 @@
+const skillsDiv = document.getElementById("skills");
+const projectsDiv = document.getElementById("projects");
+const contactDiv = document.getElementById("contact");
+const mainDiv = document.getElementById("main");
+const social = document.getElementById("bottom");
+const divide = document.getElementById("social-divider-1");
+
+function main() {
+        mainDiv.style.display = 'block';
+        skillsDiv.style.display = 'none';
+        projectsDiv.style.display = 'none';
+        contactDiv.style.display = 'none';
+        social.style.display = 'none';
+}
+
+function skills() {
+        mainDiv.style.display = 'none';
+        skillsDiv.style.display = 'block';
+        projectsDiv.style.display = 'none';
+        contactDiv.style.display = 'none';
+        social.style.display = 'none';
+}
+
+function projects() {
+        mainDiv.style.display = 'none';
+        skillsDiv.style.display = 'none';
+        projectsDiv.style.display = 'block';
+        contactDiv.style.display = 'none';
+        social.style.display = 'none';
+
+        document.querySelectorAll(".ele")[2].click();
+}
+
+function contact() {
+        mainDiv.style.display = 'none';
+        skillsDiv.style.display = 'none';
+        projectsDiv.style.display = 'none';
+        contactDiv.style.display = 'block';
+        social.style.display = 'block';
+        divide.style.display = 'none';
+}
+
+let tab = [];
+function sel(e) {
+    const act = e.currentTarget;
+
+    document.querySelectorAll(".ele").forEach(el => {
+        el.classList.remove("active");
+    });
+
+    act.classList.add("active");
+}
+
+
+document.querySelectorAll(".ele").forEach(act => {
+    act.addEventListener("click", sel);
+});
+
+document.querySelectorAll("a").forEach( link => {
+    link.addEventListener("click", () => {
+        link.classList.add("clicked");
+    });
+});
