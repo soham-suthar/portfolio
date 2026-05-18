2350bbf42214d0cca58dca01d4cc4b0ce2ee700e
soham-suthar <sohamnishithsutharrr@gmail.com>
2026-05-18 17:00:44 +0530

    Refactor main.js and style.css for improved readability and consistency
    - Updated JavaScript functions to use consistent double quotes for strings.
    - Simplified display logic in main, skills, projects, and contact functions.
    - Enhanced CSS formatting for better readability and organization.
    - Adjusted media queries for responsive design improvements.
    - Standardized color variables and hover effects for consistency across elements.

--- main.js
+++ main.js
@@ -6,57 +6,56 @@ const social = document.getElementById("bottom");
 const divide = document.getElementById("social-divider-1");
 
 function main() {
-        mainDiv.style.display = 'block';
-        skillsDiv.style.display = 'none';
-        projectsDiv.style.display = 'none';
-        contactDiv.style.display = 'none';
-        social.style.display = 'none';
+    mainDiv.style.display = "block";
+    skillsDiv.style.display = "none";
+    projectsDiv.style.display = "none";
+    contactDiv.style.display = "none";
+    social.style.display = "none";
 }
 
 function skills() {
-        mainDiv.style.display = 'none';
-        skillsDiv.style.display = 'block';
-        projectsDiv.style.display = 'none';
-        contactDiv.style.display = 'none';
-        social.style.display = 'none';
+    mainDiv.style.display = "none";
+    skillsDiv.style.display = "block";
+    projectsDiv.style.display = "none";
+    contactDiv.style.display = "none";
+    social.style.display = "none";
 }
 
 function projects() {
-        mainDiv.style.display = 'none';
-        skillsDiv.style.display = 'none';
-        projectsDiv.style.display = 'block';
-        contactDiv.style.display = 'none';
-        social.style.display = 'none';
+    mainDiv.style.display = "none";
+    skillsDiv.style.display = "none";
+    projectsDiv.style.display = "block";
+    contactDiv.style.display = "none";
+    social.style.display = "none";
 
-        document.querySelectorAll(".ele")[2].click();
+    document.querySelectorAll(".ele")[2].click();
 }
 
 function contact() {
-        mainDiv.style.display = 'none';
-        skillsDiv.style.display = 'none';
-        projectsDiv.style.display = 'none';
-        contactDiv.style.display = 'block';
-        social.style.display = 'block';
-        divide.style.display = 'none';
+    mainDiv.style.display = "none";
+    skillsDiv.style.display = "none";
+    projectsDiv.style.display = "none";
+    contactDiv.style.display = "block";
+    social.style.display = "block";
+    divide.style.display = "none";
 }
 
 let tab = [];
 function sel(e) {
     const act = e.currentTarget;
 
-    document.querySelectorAll(".ele").forEach(el => {
+    document.querySelectorAll(".ele").forEach((el) => {
         el.classList.remove("active");
     });
 
     act.classList.add("active");
 }
 
-
-document.querySelectorAll(".ele").forEach(act => {
+document.querySelectorAll(".ele").forEach((act) => {
     act.addEventListener("click", sel);
 });
 
-document.querySelectorAll("a").forEach( link => {
+document.querySelectorAll("a").forEach((link) => {
     link.addEventListener("click", () => {
         link.classList.add("clicked");
     });
@@ -68,10 +67,10 @@ function toggleMenu() {
 
 const elements = document.querySelectorAll(".ele");
 
-    elements.forEach(item => {
-        item.addEventListener("click", () => {
-            if (window.innerWidth <= 900) {
-                document.querySelector(".menu").classList.remove("active");
-            }
-        });
+elements.forEach((item) => {
+    item.addEventListener("click", () => {
+        if (window.innerWidth <= 900) {
+            document.querySelector(".menu").classList.remove("active");
+        }
     });
+});
