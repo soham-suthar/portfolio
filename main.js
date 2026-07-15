973d0d290856d4cea82a98e457d434d24938af14
soham-suthar <sohamnishithsutharrr@gmail.com>
2026-07-16 00:32:15 +0530

    Refactor code structure for improved readability and maintainability

--- main.js
+++ main.js
@@ -6,100 +6,96 @@ const social = document.getElementById("bottom");
 const divide = document.getElementById("social-divider-1");
 
 function main() {
-    mainDiv.style.display = "block";
-    skillsDiv.style.display = "none";
-    projectsDiv.style.display = "none";
-    contactDiv.style.display = "none";
-    social.style.display = "none";
+  mainDiv.style.display = "block";
+  skillsDiv.style.display = "none";
+  projectsDiv.style.display = "none";
+  contactDiv.style.display = "none";
+  social.style.display = "none";
 }
 
 function skills() {
-    mainDiv.style.display = "none";
-    skillsDiv.style.display = "block";
-    projectsDiv.style.display = "none";
-    contactDiv.style.display = "none";
-    social.style.display = "none";
+  mainDiv.style.display = "none";
+  skillsDiv.style.display = "block";
+  projectsDiv.style.display = "none";
+  contactDiv.style.display = "none";
+  social.style.display = "none";
 
-    const skillCard = document.getElementById("s_card");
+  const skillCard = document.getElementById("s_card");
 
-    let options = "";
+  let options = "";
 
-    skillCards.forEach(value => {
-    options +=  `
+  skillCards.forEach((value) => {
+    options += `
     <div class="sub"> <img src="${value.src}">
                     <div class="txt"> ${value.skill} </div>
                 </div>
     `;
-});
-    skillCard.innerHTML = options;
-    console.log(options);
-    
-
+  });
+  skillCard.innerHTML = options;
+  console.log(options);
 }
 
 function projects() {
-    mainDiv.style.display = "none";
-    skillsDiv.style.display = "none";
-    projectsDiv.style.display = "block";
-    contactDiv.style.display = "none";
-    social.style.display = "none";
+  mainDiv.style.display = "none";
+  skillsDiv.style.display = "none";
+  projectsDiv.style.display = "block";
+  contactDiv.style.display = "none";
+  social.style.display = "none";
 
-    document.querySelectorAll(".ele")[2].click();
+  document.querySelectorAll(".ele")[2].click();
 }
 
 function contact() {
-    mainDiv.style.display = "none";
-    skillsDiv.style.display = "none";
-    projectsDiv.style.display = "none";
-    contactDiv.style.display = "block";
-    social.style.display = "block";
-    divide.style.display = "none";
+  mainDiv.style.display = "none";
+  skillsDiv.style.display = "none";
+  projectsDiv.style.display = "none";
+  contactDiv.style.display = "block";
+  social.style.display = "block";
+  divide.style.display = "none";
 }
 
 const skillCards = [
-    {skill: "HTML", src: "images/html.png"},
-    {skill: "CSS", src: "images/css.png"},
-    {skill: "JavaScript", src: "images/js.png"},
-    {skill: "Node.js", src: "images/nodejs.png"},
-    {skill: "MongoDB", src: "images/Mongodb.png"},
-    {skill: "Chess", src: "images/chess.png"},
-    {skill: "Academics", src: "images/academics.png"},
-    
+  { skill: "HTML", src: "images/html.png" },
+  { skill: "CSS", src: "images/css.png" },
+  { skill: "JavaScript", src: "images/js.png" },
+  { skill: "Node.js", src: "images/nodejs.png" },
+  { skill: "Express.js", src: "images/expressjs.png" },
+  { skill: "MongoDB", src: "images/Mongodb.png" },
+  { skill: "Chess", src: "images/chess.png" },
+  { skill: "Academics", src: "images/academics.png" },
 ];
-
-
 
 let tab = [];
 function sel(e) {
-    const act = e.currentTarget;
+  const act = e.currentTarget;
 
-    document.querySelectorAll(".ele").forEach((el) => {
-        el.classList.remove("active");
-    });
+  document.querySelectorAll(".ele").forEach((el) => {
+    el.classList.remove("active");
+  });
 
-    act.classList.add("active");
+  act.classList.add("active");
 }
 
 document.querySelectorAll(".ele").forEach((act) => {
-    act.addEventListener("click", sel);
+  act.addEventListener("click", sel);
 });
 
 document.querySelectorAll("a").forEach((link) => {
-    link.addEventListener("click", () => {
-        link.classList.add("clicked");
-    });
+  link.addEventListener("click", () => {
+    link.classList.add("clicked");
+  });
 });
 
 function toggleMenu() {
-    document.querySelector(".menu").classList.toggle("active");
+  document.querySelector(".menu").classList.toggle("active");
 }
 
 const elements = document.querySelectorAll(".ele");
 
 elements.forEach((item) => {
-    item.addEventListener("click", () => {
-        if (window.innerWidth <= 900) {
-            document.querySelector(".menu").classList.remove("active");
-        }
-    });
+  item.addEventListener("click", () => {
+    if (window.innerWidth <= 900) {
+      document.querySelector(".menu").classList.remove("active");
+    }
+  });
 });
